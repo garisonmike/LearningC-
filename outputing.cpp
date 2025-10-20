d5f844fef835d047cc8c1bd22dba125008458da7
garisonmike <garisonmike006@gmail.com>
2025-10-20 12:01:27 +0300

    learnt to output and use variables in statements.

---
+++ outputing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int x = 23;
+
+  // std::cout << "The value of x is ";
+  // std::cout << x;
+  std::cout << "The value of x is " << x;
+  // I just realised i can name as .c++ or .cpp
+  return 0;
+}
